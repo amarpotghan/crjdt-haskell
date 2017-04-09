4bc6ec8897f2499881b7d3af486b185f91c3fd01
Amar Potghan <amar@riskident.com>
2017-04-09 14:11:46 +0200

    Add a data type for Cursor

--- src/Data/Crjdt/Core.hs
+++ src/Data/Crjdt/Core.hs
@@ -1,14 +1,39 @@
+{-# LANGUAGE GADTs                     #-}
 module Data.Crjdt.Core where
 
 import Data.Text
 import Data.String
+import Data.Void
+import qualified Data.Sequence as Seq
 
 data Var = Variable Text deriving (Show, Eq)
 
 instance IsString Var where
   fromString = Variable . fromString
 
-type Key = Text
+data TaggedKey tag = TK
+  { tag :: tag
+  , key :: Text
+  } deriving (Show, Eq)
+
+data Key tag where
+  Key :: Text -> Key Void
+  TaggedKey :: TaggedKey tag -> Key tag
+
+instance Show tag => Show (Key tag) where
+  show (Key t) = show t
+  show (TaggedKey taggedKey) = show taggedKey
+
+instance Eq tag => Eq (Key tag) where
+  (Key t) == (Key t1) = t == t1
+  (TaggedKey t) == (TaggedKey t1) = t == t1
+  (Key _) == (TaggedKey _) = False
+  (TaggedKey _) == (Key _) = False
+
+data Tag
+  = MapT
+  | ListT
+  deriving (Show, Eq)
 
 data Val
   = Number Int
@@ -34,5 +59,20 @@ data Expr
   | Keys Expr
   | Values Expr
   | At Expr Int
-  | GetKey Expr Key
+  | GetKey Expr (Key Void)
   deriving (Show, Eq)
+
+-- DOC
+doc :: Expr
+doc = Doc
+
+untaggedKey :: Text -> Key Void
+untaggedKey = Key
+
+tagWith :: tag -> Text -> Key tag
+tagWith t = TaggedKey . TK t
+
+data Cursor = Cursor
+  { path :: Seq.Seq (Key Tag)
+  , finalKey :: Key Void
+  } deriving (Show, Eq)
