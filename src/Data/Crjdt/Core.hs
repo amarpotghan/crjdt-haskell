97f031040cab7179bdf0b213400f8a196f689165
amar <amarpotghan@gmail.com>
2017-04-15 16:36:28 +0200

    More progress in Expr evaluation

--- src/Data/Crjdt/Core.hs
+++ src/Data/Crjdt/Core.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Data.Crjdt.Core where
@@ -14,6 +17,7 @@ import Control.Monad.Fail
 import Control.Monad.State
 import Control.Monad.Except
 import Control.Applicative
+import Data.Sequence (ViewL(..), viewl)
 import qualified Data.Sequence as Seq
 
 data Var = Variable Text deriving (Show, Eq)
@@ -30,6 +34,9 @@ data Key tag where
   Key :: Text -> Key Void
   TaggedKey :: TaggedKey tag -> Key tag
 
+instance IsString (Key Void) where
+  fromString = Key . fromString
+
 instance Show tag => Show (Key tag) where
   show (Key t) = show t
   show (TaggedKey taggedKey) = show taggedKey
@@ -43,6 +50,7 @@ instance Eq tag => Eq (Key tag) where
 data Tag
   = MapT
   | ListT
+  | RegT
   deriving (Show, Eq)
 
 data Val
@@ -68,7 +76,8 @@ data Expr
   | Var Var
   | Keys Expr
   | Values Expr
-  | At Expr Int
+  | Iter Expr
+  | Next Expr
   | GetKey Expr (Key Void)
   deriving (Show, Eq)
 
@@ -81,9 +90,6 @@ type Result = Cursor
 -- DOC
 doc :: Key Void
 doc = Key "doc"
-
-untaggedKey :: Text -> Key Void
-untaggedKey = Key
 
 tagWith :: tag -> Text -> Key tag
 tagWith t = TaggedKey . TK t
@@ -98,6 +104,10 @@ data Context = Context
 data EvalError
   = GetOnHead
   | UndefinedVariable Var
+
+headKey, tailKey :: Key Void
+headKey = "head"
+tailKey = "tail"
 
 newtype Eval a
   = Eval { runEval :: ExceptT EvalError (State Context) a }
@@ -122,7 +132,41 @@ appendWith t k (Cursor p final) = Cursor (p `mappend` Seq.singleton (doTag t fin
 lookupCtx :: Var -> Context -> Maybe Cursor
 lookupCtx _ _ = Nothing
 
-eval :: (MonadError EvalError m, MonadState Context m) => Expr -> m Result
+alreadyPresent :: Key Void -> Context -> Bool
+alreadyPresent _ _ = False
+
+next :: Key Void -> Context -> Key Void
+next _ _ = "tail"
+
+-- Avoiding Lens dependency for now
+setFinalKey :: Cursor -> Key Void -> Cursor
+setFinalKey c fk = c { finalKey = fk }
+
+setPath :: Cursor -> Seq.Seq (Key Tag) -> Cursor
+setPath c newpath = c { path = newpath }
+
+type Ctx m = (MonadError EvalError m, MonadState Context m)
+
+findChild :: Key Tag -> Context -> Maybe Document
+findChild _ _ = Nothing
+
+data Document
+
+stepNext :: Ctx m => Document -> Cursor -> m Cursor
+stepNext d c@(Cursor (viewl -> Seq.EmptyL) (next -> getNextKey)) = get >>= \ctx -> do
+  let nextKey = getNextKey ctx
+  case (nextKey /= tailKey, alreadyPresent nextKey ctx) of
+    (True, True) -> pure (Cursor Seq.empty nextKey)
+    (True, False) -> stepNext d (Cursor Seq.empty nextKey)
+    (False, _) -> pure c
+stepNext d c@(Cursor (viewl -> (x :< xs)) _) = get >>= f
+  where f = maybe (pure c) (fmap (setFinalKey c . finalKey) . (`stepNext` (setPath c xs))) . findChild x
+stepNext _ c = pure c
+
+topDoc :: Document
+topDoc = undefined
+
+eval :: Ctx m => Expr -> m Result
 eval Doc = pure $ Cursor Seq.empty doc
 eval (GetKey expr k) = do
   cursor <- eval expr
@@ -130,3 +174,7 @@ eval (GetKey expr k) = do
     (Key "head") -> throwError GetOnHead
     _ -> pure (appendWith MapT k cursor)
 eval (Var var) = get >>= maybe (throwError (UndefinedVariable var)) pure . lookupCtx var
+eval (Iter expr) = appendWith ListT headKey <$> eval expr
+eval (Next expr) = eval expr >>= stepNext topDoc
+-- TODO: query part. Not yet implemented
+eval values = eval values
