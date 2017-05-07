c090d7cb0a5cf6c51ed14140b4d571f5561bd1f3
Amar <amarpotghan@gmail.com>
2017-05-07 15:28:15 +0200

    Start documentation

--- src/Data/Crjdt.hs
+++ src/Data/Crjdt.hs
@@ -1,24 +1,41 @@
+-- |This module exports all necessarry data types and functions for
+-- expressing and executing commands which allow modifying and
+-- querying the state of JSON in local replica
+
 module Data.Crjdt
-  ( module Core
-  , bind
-  , (-<)
-  , iter
+  (
+  -- * Expressions
+    iter
   , next
   , key
   , doc
   , var
-  , insert
-  , delete
-  , emptyMap
-  , emptyList
-  , (=:)
-  , string
+
+  -- * Commands
+  , Command(..)
   , yield
   , keys
   , values
+  , insert
+  , delete
+  , bind
+  , (-<)
+  , assign
+  , (=:)
+
+  -- * Values
+  , string
+  , emptyMap
+  , emptyList
+
+  -- * Evaluation and Execution
+  , Eval.eval
+  , Eval.execute
+
+  -- * Others
   , Void
   , (.>)
-  , Command(..)
+  , module Core
   ) where
 
 import Data.Text as T
@@ -28,54 +45,90 @@ import Control.Monad.Free (liftF)
 
 import Data.Crjdt.Context as Core
 import Data.Crjdt.Types as Core
-import Data.Crjdt.Eval as Core
+
+import qualified Data.Crjdt.Eval as Eval (execute, eval)
+import Data.Crjdt.Eval as Core hiding (execute, eval)
 
 import Data.Crjdt.Internal
 
 (.>) :: b -> (b -> a) -> a
 (.>) b f = f b
 
-emptyMap, emptyList :: Val
+-- |'emptyMap' corresponds to {}.
+emptyMap :: Val
 emptyMap = EmptyObject
+
+-- |'emptyList' corresponds to [].
+emptyList :: Val
 emptyList = EmptyArray
 
+-- |Apply commands received from other replicas to current replica.
 yield :: Command ()
 yield = liftF (Yield ())
 
+{-| String literal.
+
+  @
+    'doc' '.>' "planet" '.=' 'string' \"Earth\"
+  @
+
+-}
 string :: Text -> Val
 string = StringLit
 
+{-| Starts iterating over the list.
+
+  @
+    do
+     let planetList = 'doc' '.>' "planet" .> 'iter'
+     insert \"Earth\" planetList
+  @
+
+-}
 iter :: Expr -> Expr
 iter = Iter
 
+{-| Moves to the next element in the list.
+-}
 next :: Expr -> Expr
 next = Next
 
+{-| Get value of given key in given expression.
+-}
 key :: Key Void -> Expr -> Expr
 key = flip GetKey
 
+{-| The root of the JSON object.
+-}
 doc :: Expr
 doc = Doc
 
+-- |Variable.
 var :: Text -> Expr
 var = Var . Variable
 
+-- |Insert a @Val@ in the list.
 insert :: Val -> Expr -> Command ()
 insert v e = liftF (InsertAfter e v ())
 
+-- |Delete the list element.
 delete :: Expr -> Command ()
 delete e = liftF (Delete e ())
 
+-- |Get all keys of object pointed by given 'Expr'.
 keys :: Expr -> Command (Set (Key Void))
 keys e = liftF (Keys e id)
 
+-- |Get all values of object pointed by given 'Expr'.
 values :: Expr -> Command [Val]
 values e = liftF (Values e id)
 
+-- |Assign value to the key pointed by given 'Expr'.
 assign, (=:) :: Expr -> Val -> Command ()
 assign e v = liftF (Assign e v ())
 (=:) = assign
 
+-- |Let binding.
 bind, (-<) :: Text -> Expr -> Command Expr
 bind t e = liftF (Let t e id)
 (-<) = bind
